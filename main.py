445a98e42a54692684c95871ca01b6954e514a04
Paul <paulwrath1223>
2022-01-12 21:19:13 -0800

    added methods to convert between coordinate notations

---
+++ main.py
@@ -0,0 +1,114 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def code_notation_to_chess_notation(a):
+    b1 = a[1] + 1
+    b2 = chr(a[1] + 97)
+    b = (b1, b2)
+    return b
+
+
+def chess_notation_to_code_notation(a):
+    b1 = a[1] - 1
+    b2 = ord(a[1]) - 97
+    b = (b1, b2)
+    return b
+
