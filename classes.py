a8bbe9c6dcb45d0831ca0e88d94f07c2b629f141
Paul <paulwrath1223>
2022-01-12 20:49:04 -0800

    commit

---
+++ classes.py
@@ -0,0 +1,16 @@
+
+
+class Pawn:
+        """
+
+        sus
+        amogus
+        """
+
+    def __init__(self, coordinates, white):
+
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
